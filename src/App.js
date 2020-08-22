b01e409d3e847ebcff54e987aab1bd7b596e9669
sashida <sashida@primagest.co.jp>
2020-08-22 16:01:26 +0900

    p.49-2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 
-const count = 4;
-
-function isEven() {
-  return count % 2 === 0 ? "Even" : "Odd";
-}
-
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 4
+    };
+  }
+
+  isEven() {
+    return this.state.count % 2 === 0 ? "Even" : "Odd";
+  }
 
   render = () =>
   <h4 className="bg-primary text-white text-center p-2 m-1">
-    Number of things: { isEven() }
+    Number of things: { this.isEven() }
   </h4>
 }
