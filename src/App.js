a9cc2795c57832f2ebea05a58e060687c32cb718
sashida <sashida@primagest.co.jp>
2020-08-22 16:02:10 +0900

    p.50

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ export default class App extends Component {
     };
   }
 
-  isEven() {
-    return this.state.count % 2 === 0 ? "Even" : "Odd";
+  isEven(val) {
+    return val % 2 === 0 ? "Even" : "Odd";
   }
 
   render = () =>
   <h4 className="bg-primary text-white text-center p-2 m-1">
-    Number of things: { this.isEven() }
+    Number of things: { this.isEven(this.state.count) }
   </h4>
 }
