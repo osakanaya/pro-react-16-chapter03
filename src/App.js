6927c5ae64aaede5b9f7e049f7f3a66c12637dfb
sashida <sashida@primagest.co.jp>
2020-08-22 15:59:24 +0900

    p.48-2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,6 @@ export default class App extends Component {
 
   render = () =>
   <h4 className="bg-primary text-white text-center p-2 m-1">
-    Number of things: { count }
+    Number of things: { count % 2 == 0 ? "Even" : "Odd" }
   </h4>
 }
