cccd8ed49c6d98d36fdde37202732b33751b89fb
sashida <sashida@primagest.co.jp>
2020-08-22 15:58:04 +0900

    p.47

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 
+const message = "This is a constant"
+
 export default class App extends Component {
+
   render = () =>
   <h4 className="bg-primary text-white text-center p-2 m-1">
-    Component Elements
+    { message }
   </h4>
 }
