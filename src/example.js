2fa055ec0bdc5aa8cca36c4adb2460c28caea87f
sashida <sashida@primagest.co.jp>
2020-08-24 08:30:08 +0900

    p.67

--- src/example.js
+++ src/example.js
@@ -3,10 +3,14 @@ console.log("Apples");
 console.log("This is a statement");
 console.log("This is also a statement");
 
-function myFunc(name, weather) {
+function myFunc(name, weather, ...extraArgs) {
     console.log("Hello " + name + ".");
     console.log("It is " + weather + " today.");
+
+    for(let i = 0; i < extraArgs.length; i++) {
+        console.log("Extra Arg: " + extraArgs[i]);
+    }
 }
 
-myFunc("Adam", "sunny");
+myFunc("Adam", "sunny", "one", "two", "three");
 
