cdf337ac7105abccb0945ad6707ee253ac6fb89f
sashida <sashida@primagest.co.jp>
2020-08-24 14:05:31 +0900

    p.90

--- src/example.js
+++ src/example.js
@@ -1,17 +1,7 @@
-const myData = {
-    name: "Bob",
-    location: {
-        city: "Paris",
-        country: "France"
-    },
-    employment: {
-        title: "Manager",
-        dept: "Sales"
-    }
-};
+import additionFunction from "./sum";
 
-function printDetails({ name, location: { city }, employment: { title }}) {
-    console.log(`Name: ${name}, City: ${city}, Role: ${title}`);
-}
+let values = [10, 20, 30, 40, 50];
 
-printDetails(myData);
+let total = additionFunction(values);
+
+console.log(`Total: ${total}`);
