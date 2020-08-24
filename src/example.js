958704a3625229dc283cc025e3dc7d431bc0a71e
sashida <sashida@primagest.co.jp>
2020-08-24 17:13:25 +0900

    p.95

--- src/example.js
+++ src/example.js
@@ -1,12 +1,7 @@
-import oddOnly, { sumValues } from "./sum";
-import * as ops from "./operations";
+import { asyncAdd } from "./async";
 
 let values = [10, 20, 30, 40, 50];
 
-let total = sumValues(values);
-let odds = oddOnly(values);
+let total = asyncAdd(values);
 
-console.log(`Total: ${total}, Odd Total: ${odds}`);
-
-console.log(`Multiply: ${ops.multiply(values)}`);
-console.log(`Subtract: ${ops.subtract(1000, values)}`);
+console.log(`Main Total: ${total}`);
