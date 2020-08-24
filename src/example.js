e0b074e94075b9fa042160db21b5afb37ef7b31f
sashida <sashida@primagest.co.jp>
2020-08-24 10:12:58 +0900

    p.69

--- src/example.js
+++ src/example.js
@@ -3,12 +3,8 @@ console.log("Apples");
 console.log("This is a statement");
 console.log("This is also a statement");
 
-function myFunc(nameFunction) {
-    return ("Hello " + nameFunction() + ".");
-}
+const myFunc = (nameFunction)  => ("Hello " + nameFunction() + ".");
 
-function printName(nameFunction, printFunction) {
-    printFunction(myFunc(nameFunction));
-}
+const printName = (nameFunction, printFunction) => printFunction(myFunc(nameFunction));
 
 printName(function () { return "Adam"; }, console.log);
