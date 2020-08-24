7805522a643d454d91e92eff5a92ded20e07413f
sashida <sashida@primagest.co.jp>
2020-08-24 10:24:19 +0900

    p.76

--- src/example.js
+++ src/example.js
@@ -1,29 +1,5 @@
-let name = "Adam";
+let myData1 = 5 + 5;
+let myData2 = 5 + "5";
 
-if (name === "Adam") {
-    console.log("Name is Adam");
-} else if (name === "Jacqui") {
-    console.log("Name is Jacqui");
-} else {
-    console.log("Name is neither Adam or Jacqui");
-}
-
-switch(name) {
-    case "Adam":
-        console.log("Name is Adam");
-        break;
-    case "Jacqui":
-        console.log("Name is Jacqui");
-        break;
-    default:
-        console.log("Name is neither Adam or Jacqui");
-        break;
-}
-
-let firstVal = 5;
-let secondVal = "5";
-if (firstVal === secondVal) {
-    console.log("They are the same");
-} else {
-    console.log("They are NOT the same");
-}
+console.log("Result 1: " + myData1);
+console.log("Result 2: " + myData2);
