55b441084c07d4449410ca3ca37fe2a42bcf9270
sashida <sashida@primagest.co.jp>
2020-08-24 10:23:27 +0900

    p.75-2

--- src/example.js
+++ src/example.js
@@ -22,7 +22,7 @@ switch(name) {
 
 let firstVal = 5;
 let secondVal = "5";
-if (firstVal == secondVal) {
+if (firstVal === secondVal) {
     console.log("They are the same");
 } else {
     console.log("They are NOT the same");
