69f8df0c276cd8c77d0798ef6c055bd93726219f
sashida <sashida@primagest.co.jp>
2020-08-24 10:23:13 +0900

    p.75

--- src/example.js
+++ src/example.js
@@ -19,3 +19,11 @@ switch(name) {
         console.log("Name is neither Adam or Jacqui");
         break;
 }
+
+let firstVal = 5;
+let secondVal = "5";
+if (firstVal == secondVal) {
+    console.log("They are the same");
+} else {
+    console.log("They are NOT the same");
+}
