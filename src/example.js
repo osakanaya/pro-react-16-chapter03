555778a566da93f83bde7678aa9b7d08331704a2
sashida <sashida@primagest.co.jp>
2020-08-24 17:21:23 +0900

    p.97

--- src/example.js
+++ src/example.js
@@ -2,5 +2,11 @@ import { asyncAdd } from "./async";
 
 let values = [10, 20, 30, 40, 50];
 
-asyncAdd(values).then(total => console.log(`Main Total: ${total}`));
+// asyncAdd(values).then(total => console.log(`Main Total: ${total}`));
 
+async function doTask() {
+    let total = await asyncAdd(values);
+    console.log(`Main Total: ${total}`);
+}
+
+doTask();
